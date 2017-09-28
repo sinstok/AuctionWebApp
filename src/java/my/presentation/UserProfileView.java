42bdadc97c58575662f2cfb4d5601fa7d7696651
sinstok <siddi.stokkenes@gmail.com>
2017-09-28 19:33:43 +0200

    La til min profil side, der du kan se bruker info + legge til produkter

---
+++ src/java/my/presentation/UserProfileView.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.presentation;
+
+import boundary.AuctionUserFacade;
+import entities.AuctionUser;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author Sindre
+ */
+@Named(value = "UserProfileView")
+@RequestScoped
+public class UserProfileView {
+
+    @EJB
+    private AuctionUserFacade auctionUserFacade;
+    private AuctionUser user;
+    
+    /**
+     * Creates a new instance of UserProfileView
+     */
+    public UserProfileView() {
+        user = new AuctionUser();
+    }
+
+    public String getEmail() {
+        return user.getEmail();
+    }
+
+    public String getUserName() {
+        return user.getName();
+    }
+
+    public String findUser(Long id) {
+        if (id != null) {
+            this.user = auctionUserFacade.find(id);
+            return "userProfile";
+        } else {
+            return "index?faces-redirect-true";
+        }
+    }
+
+    public AuctionUser getUser() {
+        return user;
+    }
+}
