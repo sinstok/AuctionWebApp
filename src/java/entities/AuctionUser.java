097b02112433c8badfbdd7cd837ead9f4a642658
tomfjug <tomas.fjugstaderiksen@gmail.com>
2017-10-01 12:18:05 +0200

    La til phone og adress til registreing

--- src/java/entities/AuctionUser.java
+++ src/java/entities/AuctionUser.java
@@ -108,6 +108,22 @@ public class AuctionUser implements Serializable {
         this.name = name;
     }
 
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     public Long getId() {
         return id;
     }
