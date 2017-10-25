b6063ff09a57a19aab08e0e518f99f66fc65aeae
RagnhildOppedal <ragopp@gmail.com>
2017-10-25 14:49:55 +0200

    fikset en feil

--- AuctionWebAppClientREST/src/java/auctionwebappclientrest/Main.java
+++ AuctionWebAppClientREST/src/java/auctionwebappclientrest/Main.java
@@ -19,7 +19,6 @@ public class Main {
      */
     public static void main(String[] args) {
         Client client = ClientBuilder.newClient();
-        WebTarget base = client.target("")
         
     }
     
